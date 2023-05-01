90c54077ec5d498295a850898615207cf8a8bc46
Jean Joaquim <jeangeeklee@gmail.com>
2023-05-01 19:12:32 -0300

    styled label

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import TodoContext from '../context/TodoContext';
 import DeleteIcon from '../images/icon-cross.svg';
 
 function TodoItem({ item }) {
 
     const {deleteItem, NightMode} = useContext(TodoContext);
+    const [check, setCheck] = useState(false);
+
+    const teste = (e) => {
+        if(e.target.checked) {
+            setCheck(true);
+        } else {
+            setCheck(false);
+        }
+    }
 
     return(
         <li draggable='true' className={`todo-item ${NightMode ? 'night-mode-active' : 'night-mode-disabled'}`}>
             <label className={`custom-checkbox ${NightMode ? 'night-mode-active' : 'night-mode-disabled'}`}>
-                <input type='checkbox' />
+                <input onClick={teste} type='checkbox' className='checkbox'/>
                 <div className="custom-checkmark"></div>
                 <div className={`custom-checkmark-background ${NightMode ? 'night-mode-active' : 'night-mode-disabled'}`}></div>
             </label>
-            <p>{item.description}</p>
+            <p className='item-name' style={{
+                textDecoration: `${check ? `line-through` : `none`}`,
+                opacity: `${check ? .5 : 1}`
+            }}>{item.description}</p>
             <img onClick={() => deleteItem(item.id)} src={DeleteIcon} alt="" className="delete-icon" />
         </li>
     );
--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import TodoData from '../TodoData';
 
 const TodoContext = createContext();
