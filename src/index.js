46418a4b0381f033559f6d952429428ed4592f9a
Jean Joaquim <jeangeeklee@gmail.com>
2023-06-06 15:47:44 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { TodoProvider } from './context/TodoContext';
 import ReactDOM from 'react-dom';
 import Test from './Test';
 
 
 ReactDOM.render(
-    <React.StrictMode>
+    <TodoProvider>
         <Test />
-    </React.StrictMode>
+    </TodoProvider>
 , document.getElementById('root'));
